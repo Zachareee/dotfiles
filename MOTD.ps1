0ba47ea1bbe5cc79a5246603e07267b1dd350edc
Zachareee <zacharytanhx@yahoo.com>
2025-04-29 17:30:17 +0900

    Update outdated package fetching

--- MOTD.ps1
+++ MOTD.ps1
@@ -1,3 +1,6 @@
-$outdated = choco outdated | select -skip 4
-$outdated = $outdated | select -SkipLast $(if ($outdated -match "Enjoy using Chocolatey") {5} else {2})
+$outdated = choco outdated
+$start = ($outdated | sls "Output is package name").LineNumber
+$end = ($outdated | sls "Chocolatey has determined").LineNumber
+
+$outdated = $outdated | select -Index @(($start+1)..($end-3))
 sc MOTD.txt $outdated
