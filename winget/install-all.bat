8181006495c378cb8ee833f2f37e333d96230b9d
Zachareee <zacharytanhx@yahoo.com>
2025-06-27 23:23:51 +0900

    Remove that weird pause like what was that even

--- winget/install-all.bat
+++ winget/install-all.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -noprofile "start-process powershell -verb runas ""-noprofile -command ""cd %~dp0; Get-ChildItem -Filter *.json | %% { winget import `$_.name --accept-package-agreements --accept-source-agreements }; pause"""""
+powershell -noprofile "start-process powershell -verb runas ""-noprofile -command ""cd %~dp0; Get-ChildItem -Filter *.json | %% { winget import `$_.name --accept-package-agreements --accept-source-agreements }"""""
