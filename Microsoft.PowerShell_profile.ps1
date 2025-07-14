797ebc6a4a4be7d12396817a948686c5c0122ca6
Zachareee <zacharytanhx@yahoo.com>
2025-07-14 11:29:58 +0900

    Organising code

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,21 @@
+# configuraing profile vars
 $PROFILEDIR = $PROFILE.Substring(0, $PROFILE.LastIndexOf('\'))
 oh-my-posh init pwsh --config "$PROFILEDIR\omp\theme.json" | iex
+# shell enhancers
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+# environment config
 $env:EZA_CONFIG_DIR = "$PROFILEDIR/eza"
 
-function run-eza {
+# command aliases
+set-alias eza run-eza
+
+# helper functions
+function run-eza
+{
 	param([string]$dir = ".")
 	eza.exe -ahl --git --git-repos --no-quotes --group-directories-first --icons=always $dir
 }
 
-set-alias iel iex.bat
-set-alias eza run-eza
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
